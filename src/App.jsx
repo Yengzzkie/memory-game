a0c2a49240575e48525f69e704bafd0cc8799077
Yengzzkie <gatchalian.manuel@gmail.com>
2024-05-10 20:47:01 -0400

    implement main logic of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,74 @@
-import "./App.css";
-// import Header from "./components/Header";
-import MainPanel from "./components/MainPanel";
+import { useState, useEffect } from "react";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [playedCards, setPlayedCards] = useState([]);
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  //random number generator to get random pokemon instead of just getting the default first 20 pokemons on the API
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  //fetches pokemon images
+  async function getPokemons() {
+    const pokemonURL = 'https://pokeapi.co/api/v2/pokemon/';
+    const urls = [];
+
+    for (let i = 1; i < 10; i++) {
+      const randomNumber = generateRandomNumber();
+      const url = `${pokemonURL}${randomNumber}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        console.error(`Failed to fetch data for Pokémon ${randomNumber}`);
+        continue; 
+      }
+      const data = await response.json();
+      const spriteUrl = data.sprites.front_default; 
+      urls.push(spriteUrl);
+    }
+
+    setPokemonList(urls);
+  }
+
+  // Fisher-Yates shuffle
+  function shuffleCards(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  }
+
+  function handlePlayedCard(pokemon) {
+    const { name } = pokemon;
+
+    if (playedCards.includes(name)) {
+      console.log("Game Over");
+      return; // Exit function early if the card has already been played
+    }
+
+    setPlayedCards((prevPlayedCards) => [...prevPlayedCards, name]);
+    setPokemonList((prevPokemonList) => shuffleCards(prevPokemonList));
+  }
+
   return (
     <>
-
-      <MainPanel />
+      <h1>PokeList</h1>
+      <PokemonCard pokemonList={pokemonList} handlePlayedCard={handlePlayedCard} />
+      <h2>Played Cards:</h2>
+      <ul>
+        {playedCards.map((spriteUrl, index) => (
+          <li key={index}>
+            <img src={spriteUrl} />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+
+export default function PokemonCard({ pokemonList, handlePlayedCard }) {
+  return (
+    <ul className="pokemon-card-container">
+      {pokemonList.map((spriteUrl, index) => (
+        <li key={index} onClick={() => handlePlayedCard({ name: spriteUrl })}>
+          <img src={spriteUrl} alt={`Pokemon ${index + 1}`} />
+        </li>
+      ))}
+    </ul>  
+  );
+}
